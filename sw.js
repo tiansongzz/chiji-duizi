22742488a1b8a5d44bdfc5eac42db6578d1cf857
tiansongzz <1442115585@qq.com>
2026-09-14 09:57:51 +0800

    Refresh offline app cache

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE="吃鸡搭子-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="吃鸡搭子-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+
